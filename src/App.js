60ccf7141ad9495fbee137cf2ccd85e5664c0232
JuanIgnacioMarchetto <tu@email.com>
2024-01-24 12:02:37 -0300

    24/1/2024-12:02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Board from './Board';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
+  const [players, setPlayers] = useState(['', '']);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+
+  const handlePlayerInput = (index, url) => {
+    const newPlayers = [...players];
+    newPlayers[index] = url;
+    setPlayers(newPlayers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board
+        players={players}
+        currentPlayer={currentPlayer}
+        onPlayerInput={handlePlayerInput}
+      />
+      <GlobalStyle />
     </div>
   );
 }
